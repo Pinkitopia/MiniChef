556d6c3e10492947ff56993c9a236d3eb140df81
ErnestoJM <ernesjimenez@hotmail.com>
2019-11-25 00:05:10 +0100

    arreglado los fallos del merge
    Co-Authored-By: Iván Martín <ivan-martin@users.noreply.github.com>
    Co-Authored-By: jacquesmeyns <jacquesmeyns@users.noreply.github.com>
    Co-Authored-By: ernestoojm <ernestoojm@users.noreply.github.com>
    Co-Authored-By: Lukasha707 <lukasha707@users.noreply.github.com>

--- Assets/Scripts/Ingredient.cs
+++ Assets/Scripts/Ingredient.cs
@@ -16,42 +16,6 @@ public class Ingredient : Object
      * estados iniciales según qué ingrediente. La mayoría empieza en RAW
      */
 
-    Ingredient(int id)
-    {
-        this.id = id;
-        switch (id)
-        {
-            //beef
-            case 1:
-                state = FoodState.beefState.raw;
-                break;
-            //brocoli
-            case 2:
-                state = FoodState.brocoliState.giant;
-                break;
-            //dough
-            case 3:
-                state = FoodState.doughState.raw;
-                break;
-            //onion
-            case 4:
-                state = FoodState.onionState.giantState.raw;
-                break;
-            //cheese
-            case 5:
-                state = FoodState.cheeseState.raw;
-                break;
-            //pepperoni
-            case 6:
-                state = FoodState.pepperoniState.diced;
-                break;
-                //Any other value is not admitted
-            default:
-                id = -1;
-                break;
-        }
-    }
-
     
 
 
@@ -104,24 +68,6 @@ public class Ingredient : Object
     public virtual bool cut()
     {
         return false;
-
-        switch (toolID) {
-            //pan
-            case 1:
-                break;
-            //pot
-            case 2:
-                break;
-            //oven
-            case 3:
-                break;
-            default:
-                return false;
-
-        }
-
-        //return para evitar errores de compilación, borrar antes de debugear
-        return true;
 
     }
 }
