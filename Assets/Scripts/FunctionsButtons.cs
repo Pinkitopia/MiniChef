7f77b5c9ea6e0061bd59e9e9402b0d7af2710213
ErnestoJM <ernesjimenez@hotmail.com>
2019-12-08 02:06:46 +0100

    pausemenu

---
+++ Assets/Scripts/FunctionsButtons.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FunctionsButtons : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public GameObject[] joystickImages;
+
+    public void OnPause()
+    {
+        Time.timeScale = 0;
+        pauseMenu.SetActive(true);
+        foreach (GameObject img in joystickImages)
+        {
+            img.SetActive(false);
+        }
+    }
+
+    public void OnResume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+        foreach (GameObject img in joystickImages)
+        {
+            img.SetActive(true);
+        }
+    }
+
+    public void OnBack()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && !pauseMenu.activeSelf)
+        {
+            OnPause();
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && pauseMenu.activeSelf)
+        {
+            OnResume();
+        }
+
+            
+    }
+}
---
+++ Assets/Scripts/InputBroker.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputBroker : MonoBehaviour
+{
+    static bool GetKeyDown(KeyCode keyCode)
+    {
+        return Input.GetKeyDown(keyCode);
+    }
+    
+        
+    
+}
